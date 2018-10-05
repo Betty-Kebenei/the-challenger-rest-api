b2817cae83bc2e291522533934774d19f5ed4f73
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-10-05 17:23:47 +0300

    [chore] Convert error messages to json.

--- server/controllers/monthController.js
+++ server/controllers/monthController.js
@@ -8,7 +8,7 @@ const postMonth = (req, res) => {
     let month = new Month ({ fromDate: req.body.fromDate, toDate: req.body.toDate });
     month.save( (error, month) => {
         if(error) {
-            return res.status(400).send(error.message);
+            return res.status(400).json(error.message);
         }
         return res.status(201).json({ message: 'Month form successfully created!', month });
     });
@@ -17,7 +17,7 @@ const postMonth = (req, res) => {
 const getAllMonths = (req, res) => {
     Month.find({}, (error, months) => {
         if(error) {
-            return res.status(400).send(error.message);
+            return res.status(400).json(error.message);
         }
         if(months.length > 0) {
             return res.status(200).json(months);
@@ -29,7 +29,7 @@ const getAllMonths = (req, res) => {
 const getAMonth = (req, res) => {
     Month.findById(req.params.id, (error, month) => {
         if(error) {
-            return res.status(404).send(error.message);
+            return res.status(404).json(error.message);
         }
         res.status(200).json(month);
     });
@@ -38,7 +38,7 @@ const getAMonth = (req, res) => {
 const updateAMonth = (req, res) => {
     Month.findByIdAndUpdate(req.params.id, {$set: req.body}, (error) => {
         if(error) {
-            return res.status(400).send(error.message);
+            return res.status(400).json(error.message);
         }
         res.status(200).json({ message: 'Month form successfully updated!' });
     });
