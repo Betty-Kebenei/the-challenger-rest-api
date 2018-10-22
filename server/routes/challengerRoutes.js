36ece9895235bc25eee631cc9cf23d168c6628b1
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-10-22 12:39:48 +0300

    Protect routes with token authentication.

--- server/routes/challengerRoutes.js
+++ server/routes/challengerRoutes.js
@@ -1,21 +1,23 @@
 import express from 'express';
 const router = express.Router();
 
+import tokenAuth from '../middleware/authToken';
+
 import userController from '../controllers/userController';
 import monthController from '../controllers/monthController';
-import dailyController from '../controllers/dailycontroller';
+import dailyController from '../controllers/dailyController';
 
 router.post('/signup', userController.registerUser);
 router.post('/signin', userController.loginUser);
 
-router.post('/month-form', monthController.postMonth);
-router.get('/month-form', monthController.getAllMonths);
-router.get('/month-form/:id', monthController.getAMonth);
-router.put('/month-form/:id', monthController.updateAMonth);
+router.post('/month-form', tokenAuth.tokenAuth, monthController.postMonth);
+router.get('/month-form', tokenAuth.tokenAuth, monthController.getAllMonths);
+router.get('/month-form/:id', tokenAuth.tokenAuth, monthController.getAMonth);
+router.put('/month-form/:id', tokenAuth.tokenAuth, monthController.updateAMonth);
 
-router.post('/month-form/:id/daily-data', dailyController.postDaily);
-router.get('/month-form/:id/daily-data', dailyController.getAllDailyData);
-router.get('/month-form/:id/daily-data/:id', dailyController.getADailyData);
-router.put('/month-form/:id/daily-data/:id', dailyController.updateADailyData);
+router.post('/month-form/:id/daily-data', tokenAuth.tokenAuth, dailyController.postDaily);
+router.get('/month-form/:id/daily-data', tokenAuth.tokenAuth, dailyController.getAllDailyData);
+router.get('/month-form/:id/daily-data/:id', tokenAuth.tokenAuth, dailyController.getADailyData);
+router.put('/month-form/:id/daily-data/:id', tokenAuth.tokenAuth, dailyController.updateADailyData);
 
 export default router;
