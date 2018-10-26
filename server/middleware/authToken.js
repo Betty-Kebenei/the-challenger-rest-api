6b6576f8200d8926b13e78f744a4ae3a9ecb0806
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-10-26 14:45:39 +0300

    Add a dotenv

--- server/middleware/authToken.js
+++ server/middleware/authToken.js
@@ -4,7 +4,7 @@ const tokenAuth = (req, res, next) => {
     const token = req.headers['token'];
     if(!token) return res.status(401).send({ message: 'No token provided.' });
 
-    jwt.verify(token, 'secret_key', (error, user) => {
+    jwt.verify(token, process.env.SECRET_KEY, (error, user) => {
         if(error) return res.status(401).send({ 
             auth: false, 
             message: 'Failed to authenticate token.' 
